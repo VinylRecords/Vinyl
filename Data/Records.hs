75856012df0f4590f5387a304332a232fafbe271
Alexander Altman <alexanderaltman@me.com>
2012-10-29 13:04:04 -0700

    Change lenses to Control.Lens
    Fixes #4

--- Data/Records.hs
+++ Data/Records.hs
@@ -1,9 +1,14 @@
 module Data.Records
-  ( module X ) where
+  ( module Data.Records.Lens
+  , module Data.Records.Proofs
+  , module Data.Records.Field
+  , module Data.Records.Rec
+  , module Data.Records.Relation
+  ) where
 
-import Data.Records.Lens as X
-import Data.Records.Proofs as X
-import Data.Records.Field as X
-import Data.Records.Rec as X
-import Data.Records.Relation as X
+import Data.Records.Lens
+import Data.Records.Proofs
+import Data.Records.Field
+import Data.Records.Rec
+import Data.Records.Relation
 
--- Data/Records/Lens.hs
+++ Data/Records/Lens.hs
@@ -3,54 +3,37 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Data.Records.Lens
-  ( Lens(..)
-  , modify
-  , RLens(..)
+  ( module Control.Lens
+  , RLens
   , rLens
   , rGet
   , rPut
+  , rMod
   ) where
 
-import Prelude hiding ((.), id)
 import Data.Records.Rec
 import Data.Records.Field
 import Data.Records.Proofs
 
-import Control.Category
+import Control.Lens
 
-data Lens a b =
-  Lens { get :: (a -> b)
-       , put :: (b -> a -> a)
-       }
-
-instance Category Lens where
-  id = Lens id (const id)
-  l . m = Lens (get l . get m) (\x y -> put m (put l x (get m y)) y)
-
-modify :: Lens a b -> (b -> b) -> a -> a
-modify l f x = put l (f (get l x)) x
-
-type RLens sy t = IElem (sy ::: t) fs => Lens (Rec fs) t
+type RLens sy t = forall fs. IElem (sy ::: t) fs => SimpleLens (Rec fs) t
 
 rLens :: (sy ::: t) -> RLens sy t
 rLens f = rLens' f implicitly
 
-rGet = get . rLens
-rPut = put . rLens
+rGet = view . rLens
+rPut = flip . set . rLens
+rMod = flip . over . rLens
 
 -- Records have lenses
-rLens' :: f ~ (sy ::: t) => f -> Elem f fs -> Lens (Rec fs) t
-rLens' _ Here =
-  Lens { get = \((_,x) :& xs) -> x
-       , put = \x ((k,_) :& xs) -> (k,x) :& xs
-       }
+rLens' :: forall sy fs f t. (f ~ (sy ::: t)) => f -> Elem f fs -> SimpleLens (Rec fs) t
+rLens' _ Here = lens ((\((_,x) :& xs) -> x) :: Rec fs -> t) ((\((k,_) :& xs) x -> (k,x) :& xs) :: Rec fs -> t -> Rec fs)
 rLens' f (There p) = rLensPrepend $ rLens' f p
 
-rLensPrepend :: Lens (Rec fs) t -> Lens (Rec (f ': fs)) t
-rLensPrepend (Lens g p) =
-  Lens { get = \(_ :& xs) -> g xs
-       , put = \x (a :& xs) -> a :& (p x xs)
-       }
+rLensPrepend :: forall fs f t. SimpleLens (Rec fs) t -> SimpleLens (Rec (f ': fs)) t
+rLensPrepend l = lens ((\(_ :& xs) -> view l xs) :: Rec (f ': fs) -> t) ((\(a :& xs) x -> a :& (set l x xs)) :: Rec (f ': fs) -> t -> Rec (f ': fs))
 
