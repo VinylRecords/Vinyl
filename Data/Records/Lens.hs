57489d22b668729b5593ff9e796791ed24d88d8e
Alexander Altman <alexanderaltman@me.com>
2012-10-29 14:07:07 -0700

    Removed unneeded `forall`s
    Got rid of `ScopedTypeVariables`, too

--- Data/Records/Lens.hs
+++ Data/Records/Lens.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Data.Records.Lens
   ( module Control.Lens
@@ -20,7 +19,7 @@ import Data.Records.Proofs
 
 import Control.Lens
 
-type RLens sy t = forall fs. IElem (sy ::: t) fs => SimpleLens (Rec fs) t
+type RLens sy t = IElem (sy ::: t) fs => SimpleLens (Rec fs) t
 
 rLens :: (sy ::: t) -> RLens sy t
 rLens f = rLens' f implicitly
@@ -30,10 +29,10 @@ rPut = flip . set . rLens
 rMod = flip . over . rLens
 
 -- Records have lenses
-rLens' :: forall sy fs f t. (f ~ (sy ::: t)) => f -> Elem f fs -> SimpleLens (Rec fs) t
-rLens' _ Here = lens ((\((_,x) :& xs) -> x) :: Rec fs -> t) ((\((k,_) :& xs) x -> (k,x) :& xs) :: Rec fs -> t -> Rec fs)
+rLens' :: (f ~ (sy ::: t)) => f -> Elem f fs -> SimpleLens (Rec fs) t
+rLens' _ Here = lens (\((_,x) :& xs) -> x) (\((k,_) :& xs) x -> (k,x) :& xs)
 rLens' f (There p) = rLensPrepend $ rLens' f p
 
-rLensPrepend :: forall fs f t. SimpleLens (Rec fs) t -> SimpleLens (Rec (f ': fs)) t
-rLensPrepend l = lens ((\(_ :& xs) -> view l xs) :: Rec (f ': fs) -> t) ((\(a :& xs) x -> a :& (set l x xs)) :: Rec (f ': fs) -> t -> Rec (f ': fs))
+rLensPrepend :: SimpleLens (Rec fs) t -> SimpleLens (Rec (f ': fs)) t
+rLensPrepend l = lens (\(_ :& xs) -> view l xs) (\(a :& xs) x -> a :& (set l x xs))
 
