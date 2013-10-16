7fac31436724b70f36490b1b46d7622fb5f4b351
Nathan Howell <nhowell@alphaheavy.com>
2013-10-16 08:57:56 -0700

    SingI has been dropped from GHC 7.8

--- Data/Vinyl/Field.hs
+++ Data/Vinyl/Field.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP                 #-}
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE KindSignatures      #-}
@@ -7,11 +8,19 @@
 
 module Data.Vinyl.Field where
 
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 707
+import           Data.Proxy
+#endif
 import           GHC.TypeLits
 
 -- | A field contains a key and a type.
 data (:::) :: Symbol -> * -> * where
   Field :: sy ::: t
 
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 707
+instance KnownSymbol sy => Show (sy ::: t) where
+  show Field = symbolVal (Proxy :: Proxy sy)
+#else
 instance SingI sy => Show (sy ::: t) where
   show Field = fromSing (sing :: Sing sy)
+#endif
