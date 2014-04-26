ea2e032d46670d7021c5fc00d1bf4f5f30961a10
Jonathan Sterling <jonsterling@me.com>
2014-04-26 15:16:12 -0700

    Make the lenses also singleton agnostic (!)

--- Data/Vinyl/Lens.hs
+++ Data/Vinyl/Lens.hs
@@ -15,31 +15,30 @@ import Data.Vinyl.TyFun
 import Data.Vinyl.Witnesses
 import Data.Vinyl.Idiom.Identity
 
-import Data.Singletons
 import Control.Applicative
 
 -- | Project a field from a 'Rec'.
-rGet' :: IElem r rs => Sing r -> Rec el f rs -> f (el $ r)
+rGet' :: IElem r rs => sing r -> Rec el f rs -> f (el $ r)
 rGet' r = getConst . rLens' r Const
 {-# INLINE rGet' #-}
 
 -- | Project a field from a 'PlainRec'.
-rGet :: IElem r rs => Sing r -> PlainRec el rs -> el $ r
+rGet :: IElem r rs => sing r -> PlainRec el rs -> el $ r
 rGet = (runIdentity .) . rGet'
 {-# INLINE rGet #-}
 
 -- | Set a field in a 'Rec' over an arbitrary functor.
-rPut' :: IElem r rs => Sing r -> f (el $ r) -> Rec el f rs -> Rec el f rs
+rPut' :: IElem r rs => sing r -> f (el $ r) -> Rec el f rs -> Rec el f rs
 rPut' r x = runIdentity . rLens' r (Identity . const x)
 {-# INLINE rPut' #-}
 
 -- | Set a field in a 'PlainRec'.
-rPut :: IElem r rs => Sing r -> el $ r -> PlainRec el rs -> PlainRec el rs
+rPut :: IElem r rs => sing r -> el $ r -> PlainRec el rs -> PlainRec el rs
 rPut r x = rPut' r (Identity x)
 {-# INLINE rPut #-}
 
 -- | Modify a field.
-rMod :: (IElem r rs, Functor f) => Sing r -> (el $ r -> el $ r) -> Rec el f rs -> Rec el f rs
+rMod :: (IElem r rs, Functor f) => sing r -> (el $ r -> el $ r) -> Rec el f rs -> Rec el f rs
 rMod r f = runIdentity . rLens' r (Identity . fmap f)
 {-# INLINE rMod #-}
 
@@ -51,7 +50,7 @@ rMod r f = runIdentity . rLens' r (Identity . fmap f)
 -- package,
 --
 -- > rLens' :: IElem r rs => Sing r -> Lens' (Rec rs f) (f (El r))
-rLens' :: forall r rs f g el. (IElem r rs, Functor g) => Sing r -> (f (el $ r) -> g (f (el $ r))) -> Rec el f rs -> g (Rec el f rs)
+rLens' :: forall r rs f g el sing. (IElem r rs, Functor g) => sing r -> (f (el $ r) -> g (f (el $ r))) -> Rec el f rs -> g (Rec el f rs)
 rLens' _ f = go implicitly
   where go :: Elem r rr -> Rec el f rr -> g (Rec el f rr)
         go Here (x :& xs) = fmap (:& xs) (f x)
@@ -77,7 +76,7 @@ rLens' _ f = go implicitly
 -- supported. In the parlance of the @lens@ package,
 --
 -- > rLens :: IElem r rs => Sing r -> Lens' (PlainRec el rs) (el $ r)
-rLens :: forall r rs g el. (IElem r rs, Functor g) => Sing r -> (el $ r -> g (el $ r)) -> PlainRec el rs -> g (PlainRec el rs)
+rLens :: forall r rs g el sing. (IElem r rs, Functor g) => sing r -> (el $ r -> g (el $ r)) -> PlainRec el rs -> g (PlainRec el rs)
 rLens r = rLens' r . lenser runIdentity (const Identity)
   where lenser sa sbt afb s = sbt s <$> afb (sa s)
 {-# INLINE rLens #-}
