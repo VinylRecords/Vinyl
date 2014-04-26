9724dad1e4f47b6e07365f98217742fa4aa10ff4
Jonathan Sterling <jonsterling@me.com>
2014-04-26 14:58:00 -0700

    Remove singleton stuff from subtyping relation

--- Data/Vinyl/Relation.hs
+++ Data/Vinyl/Relation.hs
@@ -20,8 +20,6 @@ import Data.Vinyl.Lens
 import Data.Vinyl.Witnesses
 import Data.Vinyl.TyFun
 
-import Data.Singletons
-
 -- | One record is a subtype of another if the fields of the latter are a
 -- subset of the fields of the former.
 class (xs :: [k]) <: (ys :: [k]) where
@@ -30,12 +28,12 @@ class (xs :: [k]) <: (ys :: [k]) where
 instance xs <: '[] where
   cast _ = RNil
 
-instance (SingI y, IElem y xs, xs <: ys) => xs <: (y ': ys) where
-  cast xs = rGet' (lookupField (implicitly :: Elem y xs) xs) xs :& cast xs
+instance (IElem y xs, xs <: ys) => xs <: (y ': ys) where
+  cast xs = ith (implicitly :: Elem y xs) xs :& cast xs
     where
-      lookupField :: SingI r => Elem r rs -> Rec el f rs -> Sing r
-      lookupField Here      (_ :& _)  = sing
-      lookupField (There p) (_ :& xs) = lookupField p xs
+      ith :: Elem r rs -> Rec el f rs -> f (el $ r)
+      ith Here (a :& _) = a
+      ith (There p) (_ :& as) = ith p as
 
 -- | If two records types are subtypes of each other, that means that they
 -- differ only in order of fields.
