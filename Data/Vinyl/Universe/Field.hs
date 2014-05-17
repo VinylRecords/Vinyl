7381fee6498a1b08bf7feed1ba539295146d9568
Jonathan Sterling <jonsterling@me.com>
2014-05-17 15:14:29 -0700

    Remove singleton thing for Field
    (Just use Proxy)

--- Data/Vinyl/Universe/Field.hs
+++ Data/Vinyl/Universe/Field.hs
@@ -11,9 +11,6 @@ import GHC.TypeLits
 
 data (sy :: k) ::: (t :: *)
 
-data SField :: * -> * where
-  SField :: KnownSymbol sy => SField (sy ::: t)
-
 data ElField :: (TyFun * *) -> * where
   ElField :: ElField el
 type instance App ElField (sy ::: t) = t
