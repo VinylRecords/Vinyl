4c8be2ffdb31678a641c9526a5537d28fecf3610
Andrew Martin <amartin@layer3com.com>
2016-01-04 21:14:58 -0500

    Document the Data.Vinyl.Functor module

--- Data/Vinyl/Functor.hs
+++ Data/Vinyl/Functor.hs
@@ -6,12 +6,36 @@
 {-# LANGUAGE PolyKinds                  #-}
 {-# LANGUAGE TypeOperators              #-}
 
-module Data.Vinyl.Functor where
+module Data.Vinyl.Functor 
+  ( -- * Introduction
+    -- $introduction
+    -- * Data Types
+    Identity(..)
+  , Thunk(..)
+  , Lift(..)
+  , Compose(..)
+  , (:.)
+  , Const(..)
+    -- * Discussion
+    
+    -- ** Example
+    -- $example
+    
+    -- ** Ecosystem
+    -- $ecosystem
+  ) where
 
-import Control.Applicative
+import Control.Applicative hiding (Const)
 import Data.Foldable
 import Data.Traversable
 import Foreign.Storable
+
+{- $introduction
+    This module provides functors and functor compositions
+    that can be used as the interpretation function for a
+    'Rec'. For a more full discussion of this, scroll down
+    to the bottom.
+-}
 
 newtype Identity a
   = Identity { getIdentity :: a }
@@ -23,6 +47,8 @@ newtype Identity a
              , Ord
              )
 
+-- | Used in place of 'Identity' to make a record
+--   lazy in its fields.
 data Thunk a
   = Thunk { getThunk :: a }
     deriving ( Functor
@@ -94,3 +120,81 @@ instance (Applicative f, Applicative g) => Applicative (Lift (,) f g) where
   pure x = Lift (pure x, pure x)
   Lift (f, g) <*> Lift (x, y) = Lift (f <*> x, g <*> y)
 
+{- $example
+    The data types in this module are used to build interpretation
+    fuctions for a 'Rec'. To build a 'Rec' that is simply a heterogeneous
+    list, use 'Identity':
+
+> myRec1 :: Rec Identity '[Int,Bool,Char]
+> myRec1 = Identity 4 :& Identity True :& Identity 'c' :& RNil
+
+    For a record in which the fields are optional, you could alternatively
+    write:
+
+> myRec2 :: Rec Maybe '[Int,Bool,Char]
+> myRec2 = Just 4 :& Nothing :& Nothing :& RNil
+
+    And we can gather all of the effects with 'rtraverse':
+
+>>> let r2 = rtraverse (fmap Identity) myRec2
+>>> :t r2
+r2 :: Maybe (Rec Identity '[Int, Bool, Char])
+>>> r2
+Nothing
+
+    If the fields only exist once an environment is provided, you can 
+    build the record as follows:
+
+> myRec3 :: Rec ((->) Int) '[Int,Bool,Char]
+> myRec3 = (+5) :& (const True) :& (head . show) :& RNil
+
+    And again, we can collect these effects with "rtraverse":
+
+>>> (rtraverse (fmap Identity) myRec3) 8
+{13, True, '8'}
+
+    If you want the composition of these two effects, you can use "Compose":
+
+> myRec4 :: Rec (Compose Maybe ((->) Int)) '[Int,Char]
+> myRec4 = (Compose safeSqrt) :& (Compose $ safeHead . show) :& RNil
+
+-}
+
+{- $ecosystem
+    Of the five data types provided by this modules, three can
+    be found in others places: "Identity", "Compose", and "Const".
+    They are included with "vinyl" to help keep the dependency
+    list small. The differences will be discussed here.
+
+    The "Data.Functor.Identity" module was originally provided
+    by "transformers". When GHC 7.10 was released, it was moved
+    into "base-4.8". The "Identity" data type provided by that
+    module is well recognized across the haskell ecosystem
+    and has typeclass instances for lots of common typeclasses. 
+    The significant difference between it and the copy of
+    it provided here is that this one has a different 'Show'
+    instance. This is illustrated below:
+
+>>> import qualified Data.Vinyl.Functor as Vinyl
+>>> import qualified Data.Functor.Identity as Base
+>>> Vinyl.Identity "hello"
+"hello"
+>>> Base.Identity "hello"
+Identity "hello"
+
+    This 'Show' instance makes records look nicer in GHCi.
+    Feel free to use "Data.Functor.Identity" if you do not
+    need the prettier output or if you need the many additional
+    typeclass instances that are provided for the standard
+    "Identity".
+
+    The story with "Compose" and "Const" is much more simple.
+    These also exist in "transformers", although "Const" 
+    is named "Constant" there. Prior to the release of 
+    "transformers-0.5", they were not polykinded, making 
+    them unusable for certain universes. However, in 
+    "transformers-0.5" and forward, they have been made
+    polykinded. This means that they are just as usable with 'Rec'
+    as the vinyl equivalents but with many more typeclass
+    instances such as 'Ord' and 'Show'.
+-}
