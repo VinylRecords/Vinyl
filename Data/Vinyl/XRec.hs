e653f6f2c3d319bdd31e1a1bf54000617dfa468f
Anthony Cowley <acowley@gmail.com>
2018-05-01 14:19:16 -0400

    XRec API changes
    XRec is a type synonym: `type XRec f = Rec (XData f)`, and `HKD` is an
    associated type family of the `IsoHKD` type class.
    
    This is better in that `HKD` is now an open type family. It is worse
    if every other interpretation functor uses the default instance. In
    that case, the need to write `instance IsoHKD MyType where` for every
    `MyType` is a nuisance we avoided with the closed type family's
    catch-all case.

--- Data/Vinyl/XRec.hs
+++ Data/Vinyl/XRec.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -22,68 +29,76 @@
 module Data.Vinyl.XRec where
 import Data.Vinyl.Core (Rec(..))
 import Data.Vinyl.Functor
+import Data.Monoid
 import GHC.TypeLits (KnownSymbol)
 
--- | The so-called /higher-kinded data/ type family. See <http://reasonablypolymorphic.com/blog/higher-kinded-data>
---
--- The mappings provided are:
--- @'Identity' a ==> a@
--- @('Compose' f g) a ==> f (g a)@
--- @'Field' a ==> a@
-type family HKD (f :: u -> *) (t :: u) :: * where
-  HKD Identity a = a
-  HKD (Compose f g) a = HKD f (HKD g a)
-  HKD ElField '(s,a) = a
-  HKD f a = f a
+type XRec f = Rec (XData f)
+pattern (::&) :: HKD f r -> XRec f rs -> XRec f (r ': rs)
+pattern x ::& xs = XData x :& xs
+{-# COMPLETE (::&) #-}
 
--- | A 'Rec' whose fields have common context clutter eliminated by
--- 'HKD'.
-data XRec :: (u -> *) -> [u] -> * where
-  XRNil :: XRec f '[]
-  (::&) :: !(HKD f r) -> !(XRec f rs) -> XRec f (r ': rs)
 infixr 7 ::&
 
--- | Mapping each HKD functor to a symbol allows us to have a clear
--- catch-all case in a closed type family. Without this restriction to
--- a finite type, we would require overlapping type class instances.
-data IsoHKDSym = IsoId | IsoComp | IsoElF | IsoNull
+pattern XRNil :: XRec f '[]
+pattern XRNil = RNil
+{-# COMPLETE XRNil #-}
 
--- | Map a record context to the appropriate 'IsoHKDSym'
-type family HasIsoHKDSym (t :: k) :: IsoHKDSym where
-  HasIsoHKDSym Identity = 'IsoId
-  HasIsoHKDSym (Compose f g) = 'IsoComp
-  HasIsoHKDSym ElField = 'IsoElF
-  HasIsoHKDSym f = 'IsoNull
+-- | Like 'rmap', but the supplied function is written against the
+-- 'HKD'-simplified types. This is 'xrmap' sandwiched in between
+-- 'fromXRec' and 'toXRec'.
+rmapX :: forall f g rs. (XRMap f g rs, IsoXRec f rs, IsoXRec g rs)
+      => (forall a. HKD f a -> HKD g a) -> Rec f rs -> Rec g rs
+rmapX f = fromXRec . xrmapAux aux . toXRec
+  where aux :: forall a. XData f a -> XData g a
+        aux = XData . f @a . unX
 
--- | Witnesses for the isomorphisms between type constructors and
--- their HKD mappings.
-class HasIsoHKDSym f ~ tag => IsoHKD (tag :: IsoHKDSym) f a where
-  unHKD :: HKD f a -> f a
-  toHKD :: f a -> HKD f a
+-- | This is 'rmapX' specialized to a type at which it does not change
+-- interpretation functor. This can help with type inference.
+rmapXEndo :: forall f rs. (XRMap f f rs, IsoXRec f rs)
+          => (forall a. HKD f a -> HKD f a) -> Rec f rs -> Rec f rs
+rmapXEndo f = fromXRec . xrmapAux aux . toXRec
+  where aux :: forall a. XData f a -> XData f a
+        aux = XData . f @a . unX
 
--- | We freely move between @Identity x@ and @x@.
-instance IsoHKD 'IsoId Identity a where
-  unHKD = Identity
-  toHKD = getIdentity
+-- | This is 'rmap' for 'XRec'. We apply a natural transformation
+-- between interpretation functors to transport a record value between
+-- interpretations.
+xrmap :: forall f g rs. XRMap f g rs
+      => (forall a. HKD f a -> HKD g a) -> XRec f rs -> XRec g rs
+xrmap f = xrmapAux aux
+  where aux :: forall a. XData f a -> XData g a
+        aux = XData . f @a . unX
 
--- | We freely move between @(f :. g) x@ and @f (g x)@.
-instance ( IsoHKD (HasIsoHKDSym f) f (HKD g a)
-         , IsoHKD (HasIsoHKDSym g) g a
-         , Functor f)
-         => IsoHKD 'IsoComp (Compose f g) a where
-  unHKD x = Compose (unHKD <$> unHKD x)
-  toHKD (Compose fgx) = toHKD (toHKD <$> fgx)
+-- | A wrapper for an 'HKD'-simplified value. That is, noisy value
+-- constructors like 'Identity' and 'Compose' are ellided. This is
+-- used in the 'xrmapAux' type class method, but may be ignored by
+-- users whose needs are met by 'xrmap' and 'rmapX'.
+newtype XData t a = XData { unX :: HKD t a }
 
--- | We freely move between @Field x@ and @x@.
-instance KnownSymbol s => IsoHKD 'IsoElF ElField '(s,a) where
-  unHKD = Field
-  toHKD (Field x) = x
+-- | The implementation of 'xrmap' is broken into a type class to
+-- permit unrolling of the recursion across a record. The function
+-- mapped across the vector hides the 'HKD' type family under a newtype
+-- constructor to help the type checker.
+class XRMap (f :: u -> *) (g :: u -> *) (rs :: [u]) where
+  xrmapAux :: (forall (a :: u) . XData f a -> XData g a) -> XRec f rs -> XRec g rs
 
--- | All other contexts are opaque; this is reflexivity: we move
--- between @f x@ and @f x@.
-instance (HKD f a ~ f a, HasIsoHKDSym f ~ 'IsoNull) => IsoHKD 'IsoNull f a where
-  unHKD = id
-  toHKD = id
+instance XRMap f g '[] where
+  xrmapAux _ RNil = RNil
+
+instance forall f g r rs. (XRMap f g rs, IsoHKD f r, IsoHKD g r)
+  => XRMap f g (r ': rs) where
+  xrmapAux f (x :& xs) = f x :& xrmapAux f xs
+
+-- | Like 'rapply': record of components @f r -> g r@ may be applied
+-- to a record of @f@ to get a record of @g@.
+class XRApply f g rs where
+  xrapply :: XRec (Lift (->) f g) rs -> XRec f rs -> XRec g rs
+
+instance XRApply f g '[] where
+  xrapply RNil RNil = RNil
+
+instance XRApply f g rs => XRApply f g (r ': rs) where
+  xrapply (XData f :& fs) (XData x :& xs) = XData (f x) :& xrapply fs xs
 
 -- | Conversion between 'XRec' and 'Rec'. It is convenient to build
 -- and consume 'XRec' values to reduce syntactic noise, but 'Rec' has
@@ -94,9 +109,74 @@ class IsoXRec f ts where
   toXRec :: Rec f ts -> XRec f ts
 
 instance IsoXRec f '[] where
-  fromXRec XRNil = RNil
+  fromXRec RNil = RNil
   toXRec RNil = XRNil
 
-instance (IsoXRec f ts, IsoHKD (HasIsoHKDSym f) f t) => IsoXRec f (t ': ts) where
+instance (IsoXRec f ts, IsoHKD f t) => IsoXRec f (t ': ts) where
   fromXRec (x ::& xs) = unHKD x :& fromXRec xs
   toXRec (x :& xs) = toHKD x ::& toXRec xs
+
+-- | Isomorphism between a syntactically noisy value and a concise
+-- one. For types like, 'Identity', we prefer to work with values of
+-- the underlying type without writing out the 'Identity'
+-- constructor. For @'Compose' f g a@, aka @(f :. g) a@, we prefer to
+-- work directly with values of type @f (g a)@.
+--
+-- This involves the so-called /higher-kinded data/ type family. See
+-- <http://reasonablypolymorphic.com/blog/higher-kinded-data> for more
+-- discussion.
+class IsoHKD (f :: u -> *) (a :: u) where
+  type HKD f a
+  type HKD f a = f a
+  unHKD :: HKD f a -> f a
+  default unHKD :: HKD f a ~ f a => HKD f a -> f a
+  unHKD = id
+  toHKD :: f a -> HKD f a
+  default toHKD :: (HKD f a ~ f a) => f a -> HKD f a
+  toHKD = id
+
+-- | Work with values of type 'Identity' @a@ as if they were simple of
+-- type @a@.
+instance IsoHKD Identity a where
+  type HKD Identity a = a
+  unHKD = Identity
+  toHKD (Identity x) = x
+
+-- | Work with values of type 'ElField' @'(s,a)@ as if they were of
+-- type @a@.
+instance KnownSymbol s => IsoHKD ElField '(s,a) where
+  type HKD ElField '(s,a) = a
+  unHKD = Field
+  toHKD (Field x) = x
+
+-- | Work with values of type 'Compose' @f g a@ as if they were of
+-- type @f (g a)@.
+instance (IsoHKD f (HKD g a), IsoHKD g a, Functor f) => IsoHKD (Compose f g) a where
+  type HKD (Compose f g) a = HKD f (HKD g a)
+  unHKD x = Compose (unHKD <$> unHKD x)
+  toHKD (Compose fgx) = toHKD (toHKD <$> fgx)
+
+-- | Work with values of type 'Lift' @(->) f g a@ as if they were of
+-- type @f a -> g a@.
+instance (IsoHKD f a, IsoHKD g a) => IsoHKD (Lift (->) f g) a where
+  type HKD (Lift (->) f g) a = HKD f a -> HKD g a
+  unHKD x = Lift (unHKD . x . toHKD)
+  toHKD (Lift x) = toHKD . x . unHKD
+
+instance IsoHKD IO a where
+instance IsoHKD (Either a) b where
+instance IsoHKD Maybe a where
+instance IsoHKD First a where
+instance IsoHKD Last a where
+
+-- | Work with values of type 'Sum' @a@ as if they were of type @a@.
+instance IsoHKD Sum a where
+  type HKD Sum a = a
+  unHKD = Sum
+  toHKD (Sum x) = x
+
+-- | Work with values of type 'Product' @a@ as if they were of type @a@.
+instance IsoHKD Product a where
+  type HKD Product a = a
+  unHKD = Product
+  toHKD (Product x) = x
