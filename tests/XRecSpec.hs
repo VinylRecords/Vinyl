a69915a6ed2460793b7b77bec80bab4516885a8f
Anthony Cowley <acowley@gmail.com>
2018-09-06 16:03:18 -0400

    Model default function arguments

--- tests/XRecSpec.hs
+++ tests/XRecSpec.hs
@@ -11,6 +11,12 @@ import Test.Hspec (SpecWith, describe, it, shouldBe)
 foo :: FieldRec '["name" ::: String, "age" ::: Int] -> Int
 foo (ruple -> (name, age)) = length name + age
 
+-- | Like 'foo', but has default values for each parameter.
+foo' :: (RMap ss, ss ⊆ '["name" ::: String, "age" ::: Int])
+     => Rec ElField ss -> Int
+foo' = foo . withDefaults defs
+  where defs = fieldRec (#name =: "roberta", #age =: (48 :: Int))
+
 spec :: SpecWith ()
 spec = do
   describe "Named Arguments" $ do
@@ -21,6 +27,17 @@ spec = do
                      , #isAwesome =: True
                      , #name =: "Cheryl"))
       `shouldBe` 29
+
+  describe "Default arguments" $ do
+    it "Can take zero arguments" $
+      foo' (fieldRec ()) `shouldBe` 55
+    it "Can take a subset of named arguments (1)" $
+      foo' (#age =:= (39::Int)) `shouldBe` 46
+    it "Can take a subset of named arguments (2)" $
+      foo' (#name =:= "Jerome") `shouldBe` 54
+    it "Can take all arguments" $
+      foo' (fieldRec (#age =: (36::Int), #name =: "Jerome")) `shouldBe` 42
+
   describe "Can get fields through HKD" $ do
     let myRec :: Rec (Maybe :. ElField) ["name" ::: String, "age" ::: Int]
         myRec = xrec (Just "joe", Just 23)
