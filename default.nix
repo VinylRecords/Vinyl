110ee4c3456b5960d2c48dc992b5ecc37167111c
Anthony Cowley <acowley@gmail.com>
2021-03-16 12:50:21 -0400

    Updated nix packaging

--- default.nix
+++ default.nix
@@ -1,27 +1,20 @@
-{ mkDerivation, array, base, criterion, doctest, ghc-prim, hspec
-, lens, linear, microlens, mwc-random, primitive
-, should-not-typecheck, singletons, stdenv, tagged, vector
-, aeson, text, mtl, unordered-containers, lens-aeson
+{ mkDerivation, aeson, array, base, criterion, doctest, ghc-prim
+, hspec, lens, lens-aeson, lib, linear, microlens, mtl, mwc-random
+, primitive, should-not-typecheck, singletons, tagged, text
+, unordered-containers, vector
 }:
-let whitelistSource = src: allowedPrefixes:
-      builtins.filterSource
-        (path: type:
-          stdenv.lib.any (allowedPrefix: stdenv.lib.hasPrefix (toString (src + "/${allowedPrefix}")) path)
-                  allowedPrefixes)
-                  src;
-in mkDerivation {
+mkDerivation {
   pname = "vinyl";
-  version = "0.13.0";
-  src = whitelistSource ./. [ "Data" "vinyl.cabal" "LICENSE" "Setup.hs" "test" "benchmarks" ];
+  version = "0.13.1";
+  src = ./.;
   libraryHaskellDepends = [ array base ghc-prim ];
   testHaskellDepends = [
-    base doctest hspec lens microlens should-not-typecheck singletons
-    aeson text mtl unordered-containers lens-aeson
+    aeson base doctest hspec lens lens-aeson microlens mtl
+    should-not-typecheck singletons text unordered-containers vector
   ];
   benchmarkHaskellDepends = [
     base criterion linear microlens mwc-random primitive tagged vector
   ];
-  doBenchmark = true;
   description = "Extensible Records";
-  license = stdenv.lib.licenses.mit;
+  license = lib.licenses.mit;
 }
