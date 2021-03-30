1b3b3f150d09457e70b6a3c80c2ce9672b291066
Anthony Cowley <acowley@gmail.com>
2021-03-30 10:49:03 -0400

    Run CI on pushes to any branch

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -1,9 +1,14 @@
+{-# language CPP #-}
 import Test.DocTest
 
 main :: IO ()
 main = doctest [ "-package lens"
                , "-package doctest"
+#if __GLASGOW_HASKELL__ >= 900            
+               , "-package singletons-th"
+#else
                , "-package singletons"
+#endif
                , "tests/Intro.lhs"
                , "Data/Vinyl/Functor.hs"
                , "Data/Vinyl/Curry.hs" ]
